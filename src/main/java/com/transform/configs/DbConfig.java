229bbeac8613224a29ff6b2092ebee3de884948e
Matías Panza <panzamatiasezequiel@gmail.com>
2019-10-25 12:13:49 -0300

    configurada la db

--- src/main/java/com/transform/configs/DbConfig.java
+++ src/main/java/com/transform/configs/DbConfig.java
@@ -1,5 +1,31 @@
 package com.transform.configs;
 
 public class DbConfig {
+	private MongoClientURI url;
+	private MongoClient mongoClient;
+	private MongoDatabase database;
+
+	private static DbConfig instancia;
+
+	private DbConfig(){
+		url = new MongoClientURI(
+				"mongodb+srv://transform:1234@cluster0-tcjsq.mongodb.net/transform?retryWrites=true&w=majority");
+		mongoClient = new MongoClient(url);
+		database = mongoClient.getDatabase("transform");
+	}
+
+	
+	//Singleton para realizar la conexión solo una vez
+	public static DbConfig getConfiguration(){
+		if(instancia == null){
+			instancia = new DbConfig();
+		}
+		return instancia;
+	}
+	
+	//Método para obtener la DB, sobre la cual se opera
+	public MongoDatabase getDB(){
+		return database;
+	}
 
 }
